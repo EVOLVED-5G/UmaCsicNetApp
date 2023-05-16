91e4a057f0fdf3f64940611c19b42e97611e66e3
Carlos Andreo <c.andreo@uma.es>
2023-05-16 10:08:44 +0200

    Delete comments

--- src/__init__.py
+++ src/__init__.py
@@ -6,12 +6,10 @@ from flask_migrate import Migrate
 from flask_marshmallow import Marshmallow
 from flask_mail import Mail
 
-
 db = SQLAlchemy()
 migrate = Migrate()
 ma = Marshmallow()
 mail = Mail()
-
 
 def create_app(config=Config):
     app = Flask(__name__)
@@ -22,16 +20,7 @@ def create_app(config=Config):
     ma.init_app(app)
     mail.init_app(app)
     
-    
     from src.api.resources import bp_api
     app.register_blueprint(bp_api)
 
-    #from src.errors import bp as bp_errors
-    #app.register_blueprint(bp_errors)
-
-    # from src.thread import check_data
-    # check_data.process(app)
-
     return app
-
-
