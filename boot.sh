028d7bbbaa93a998b615f8377ce264f912e9d33c
Carlos Andreo <c.andreo@uma.es>
2023-07-13 15:51:01 +0200

    Add environment mode

--- boot.sh
+++ boot.sh
@@ -5,7 +5,7 @@ jq -r .capif_http_port=\"$CAPIFHTTP\" capif_registration.json >> tmp.json && mv 
 jq -r .capif_https_port=\"$CAPIFHTTPS\" capif_registration.json >> tmp.json && mv tmp.json capif_registration.json
 jq -r .capif_callback_url=\"http://$CALLBACK_ADDRESS\" capif_registration.json >> tmp.json && mv tmp.json capif_registration.json
 
-evolved5g register-and-onboard-to-capif --config_file_full_path="/usr/src/app/capif_registration.json" --environment="development"
+evolved5g register-and-onboard-to-capif --config_file_full_path="/usr/src/app/capif_registration.json" --environment="$ENVIRONMENT_MODE"
 
 flask db init
 flask db migrate -m "First migration"
