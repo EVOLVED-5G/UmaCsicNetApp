b4f093ce697433835c7da56762d30a341a66fa25
Carlos Andreo <c.andreo@uma.es>
2023-08-05 11:38:37 +0200

    Import thread for send msg via telegram to notify

--- src/__init__.py
+++ src/__init__.py
@@ -4,14 +4,12 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_marshmallow import Marshmallow
-from flask_mail import Mail
 from flask_cors import CORS
 
 # Init database
 db = SQLAlchemy()
 migrate = Migrate()
 ma = Marshmallow()
-mail = Mail()
 
 # Create function init
 def create_app(config=Config):
@@ -22,9 +20,11 @@ def create_app(config=Config):
     db.init_app(app)
     migrate.init_app(app, db)
     ma.init_app(app)
-    mail.init_app(app)
     
     from src.api.resources import bp_api
     app.register_blueprint(bp_api)
+
+    from src.thread import check_data
+    check_data.process(app)
 
     return app
